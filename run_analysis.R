45fba0accb226eaf2d407eddf9e2998c66e5d095
Martin B <buehlemj@hotmail.com>
2021-10-13 12:40:19 +0200

    Reworked run.extract to contain all relevant columns (mean and std). Before only the first 6 columns were used.

--- run_analysis.R
+++ run_analysis.R
@@ -18,28 +18,33 @@ library(dplyr)
 ## function to download source data, extract and name the relevant columns
 run.extract <- function() {
 
-  ##Download and unzip the data
+  ## Download and unzip the data
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Dataset.zip", method = "curl")
   unzip("Dataset.zip")
   
-  ##Extract the data into individual dataframes  
-  df.test.subjects    <- read.csv("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
-  df.test.activities  <- read.csv("UCI HAR Dataset/test/y_test.txt",       header = FALSE)
-  df.test.data        <- read.fwf("UCI HAR Dataset/test/X_test.txt", c(16,16,16,16,16,16))
+  ## Extract the data into individual dataframes  
+  
+  df.test.data        <- read.table("UCI HAR Dataset/test/X_test.txt")
+  df.test.subjects    <- read.csv  ("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+  df.test.activities  <- read.csv  ("UCI HAR Dataset/test/y_test.txt",       header = FALSE)
   df.test             <- cbind(df.test.subjects, df.test.activities, df.test.data)
-  df.train.subjects   <- read.csv("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
-  df.train.activities <- read.csv("UCI HAR Dataset/train/y_train.txt",       header = FALSE)
-  df.train.data       <- read.fwf("UCI HAR Dataset/train/X_train.txt", c(16,16,16,16,16,16))
+  
+  df.train.data       <- read.table("UCI HAR Dataset/train/X_train.txt")
+  df.train.subjects   <- read.csv  ("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+  df.train.activities <- read.csv  ("UCI HAR Dataset/train/y_train.txt",       header = FALSE)
   df.train <- cbind(df.train.subjects, df.train.activities, df.train.data)
   
   ## Merge the Test and Training datasets
   df <- rbind(df.test, df.train)
 
   ## Name the columns
-  names(df) <- c("Subject","Activity ID",
-                 "Mean (x-axis acceleration)","Mean (y-axis acceleration)","Mean (z-axis acceleration)",
-                 "Standard Deviation (x-axis acceleration)","Standard Deviation (y-axis acceleration)","Standard Deviation (z-axis acceleration)")
-  
+  df.columns<- read.table("UCI HAR Dataset/features.txt")[,2]
+  names(df)  <- c("Subject","Activity ID", df.columns)
+
+  # Create a logical vector to show the columns that contain Mean, mean or std
+  df.columns.match <- grepl("(mean|Mean|std)", df.columns)
+  df <- df[, c(TRUE, TRUE, df.columns.match)]
+
   ## Return the dataframe
   df
 }
@@ -87,8 +92,10 @@ run.df2 <- function(df) {
 
 
 ## Generate the two datasets
-df1 <- run.df1()
-df2 <- run.df2(df1)
+#df1 <- run.df1()
+#df2 <- run.df2(df1)
 
+## Generate the text file for submission
+#write.table(df2, "DataSet.txt", row.names = FALSE)
 
 
